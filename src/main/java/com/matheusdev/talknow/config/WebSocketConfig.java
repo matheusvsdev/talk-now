d54ea06c98319c8e49ab518e8de5d2fb5d2ff5cf
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2024-12-10 13:41:37 -0300

    Implementation of the WebSocketConfig configuration class

---
+++ src/main/java/com/matheusdev/talknow/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.matheusdev.talknow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler(), "/ws").addInterceptors(new ChatInterceptor());
+    }
+
+    @Bean
+    public ChatHandler chatHandler() {
+        return new ChatHandler();
+    }
+}
